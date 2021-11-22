90d706a8a39555f12546069c826c7ef9adb5b6b3
wwwalohacampus <h850415@naver.com>
2021-11-22 17:40:41 +0900

    애니메이션 변경

--- Day04/01_jQuery기본/js/04_animate.js
+++ Day04/01_jQuery기본/js/04_animate.js
@@ -4,85 +4,67 @@ $(function() {
 
     /* item1 */
     $('#item1 .hide').on('click', function() {
-        $('#item1 .inner').fadeOut();
+        $('#item1 .target-box').fadeOut();
     });
 
     $('#item1 .show').on('click', function() {
-        $('#item1 .inner').show();
+        $('#item1 .target-box').show();
     });
 
     $('#item1 .hide1000').on('click', function() {
-        $('#item1 .inner').hide(1000);
+        $('#item1 .target-box').hide(1000);
     });
 
     $('#item1 .show1000').on('click', function() {
-        $('#item1 .inner').show(1000);
+        $('#item1 .target-box').show(1000);
     });
 
 
-    /* item2 */
-    $('#item2 .fadeIn').on('click', function() {
-        // $('#item2 .inner').fadeIn(3000);
-        // $('#item2 .inner').fadeIn('slow');
-        $('#item2 .inner').fadeIn();
+     /* item2 */
+     $('#item2 .fadeIn').on('click', function() {
+        // $('#item2 .target-box').fadeIn(3000);
+        // $('#item2 .target-box').fadeIn('slow');
+        $('#item2 .target-box').fadeIn();
     });
 
     $('#item2 .fadeOut').on('click', function() {
-        // $('#item2 .inner').fadeOut(3000);
-        // $('#item2 .inner').fadeOut('slow');
-        $('#item2 .inner').fadeOut();
+        // $('#item2 .target-box').fadeOut(3000);
+        // $('#item2 .target-box').fadeOut('slow');
+        $('#item2 .target-box').fadeOut();
     });
 
     $('#item2 .fadeToggle').on('click', function() {
-        // $('#item2 .inner').fadeToggle(3000);
-        // $('#item2 .inner').fadeToggle('slow');
-        $('#item2 .inner').fadeToggle();
+        // $('#item2 .target-box').fadeToggle(3000);
+        // $('#item2 .target-box').fadeToggle('slow');
+        $('#item2 .target-box').fadeToggle();
     });
 
     $('#item2 .fadeTo').on('click', function() {
-        $('#item2 .inner').fadeTo('slow', 0.2);
+        $('#item2 .target-box').fadeTo('slow', 0.2);
     });
 
     /* item3 */
     $('#item3 .slideDown').on('click', function() {
-        $('#item3 .inner').slideDown();
+        $('#item3 .target-box').slideDown();
     });
 
     $('#item3 .slideUp').on('click', function() {
-        $('#item3 .inner').slideUp();
+        $('#item3 .target-box').slideUp();
     });
 
     $('#item3 .slideToggle').on('click', function() {
-        $('#item3 .inner').slideToggle();
-    });
-
-
-    $('#item3 .slideToggle').on('click', function() {
-        $('#item3 .inner').slideToggle();
+        $('#item3 .target-box').slideToggle();
     });
 
 
     /* item4 */
     $('#item4 .animate').on('click', function() {
         // .animate( CSS속성, 지속시간 );
-        $('#item4 .inner').animate({
-            'left': '400px',
-            'opacity': '0.5',
-        }, 5000);
+        $('#item4 .target-box').animate({
+            'opacity': '0.3',
+            'width': '400px',
+        }, 1000);
     });
-
-    /* item5 */
-    $('#item5')
-        .on('mouseover', function() {
-            $('#item5 .inner').stop().animate({
-                'opacity': '0.1'
-            }, 1000)
-        })
-        .on('mouseout', function() {
-            $('#item5 .inner').stop().animate({
-                'opacity': '0.9'
-            }, 1000)
-        })
 
 
 
