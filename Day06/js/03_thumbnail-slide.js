1448e1e185fcbf3e9393b36319c14e5a3b72019d
wwwalohacampus <h850415@naver.com>
2021-11-30 23:10:40 +0900

    썸네일 슬라이드 수정

--- Day06/js/03_thumbnail-slide.js
+++ Day06/js/03_thumbnail-slide.js
@@ -2,11 +2,19 @@ let slideIndex = 1;
 
 $(function() {
 
-
+    // 슬라이드 쇼 실행
     showSlides( slideIndex )
 
     
 })
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+  
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
 // 슬라이드 함수
 function showSlides(n) {
@@ -18,15 +26,15 @@ function showSlides(n) {
     if( n < 1 ) { slideIndex = slides.length }
 
     for( let i = 0 ; i < slides.length ; i++ ) {
-        slides[i].css( { display : 'none' } )
+        $(slides[i]).css({ display: 'none'} )
     }
 
     for( let i = 0 ; i < dots.length ; i++ ) {
-        dots[i].removeClass('active')
+        $(dots[i]).removeClass('active')
     }
 
-    slides[slideIndex-1].css( { display : 'block' })
-    dots[slideIndex-1].addClass('active')
-    captionText.html( dots[slideIndex-1].alt )
+    $(slides[slideIndex-1]).css( { display : 'block' })
+    $(dots[slideIndex-1]).addClass('active')
+    captionText.text( $(dots[slideIndex-1]).attr('alt') )
 
 }
